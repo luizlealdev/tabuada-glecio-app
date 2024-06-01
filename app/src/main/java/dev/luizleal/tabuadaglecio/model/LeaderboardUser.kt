067b2818ac88c00bc61bb2d08266afad31a772a7
luizlealdev <luizandreleal100@gmail.com>
2024-06-01 18:33:26 -0300

    feat: leaderboardUser class model

---
+++ app/src/main/java/dev/luizleal/tabuadaglecio/model/LeaderboardUser.kt
@@ -0,0 +1,8 @@
+package dev.luizleal.tabuadaglecio.model
+
+data class LeaderboardUser(
+    val username: String? = null,
+    val userClass: String? = null,
+    val score: Int? = null,
+    val avatarId: String? = null
+)
