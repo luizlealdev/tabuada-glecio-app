8c39e55ca33a1b5fd188d7ff70340c839155507e
luizlealdev <luizandreleal100@gmail.com>
2024-06-06 17:23:15 -0300

    fix: check the current app theme

--- app/src/main/java/dev/luizleal/tabuadaglecio/ui/MainActivity.kt
+++ app/src/main/java/dev/luizleal/tabuadaglecio/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import dev.luizleal.tabuadaglecio.R
+import dev.luizleal.tabuadaglecio.content.Theme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Theme(applicationContext).applyTheme()
     }
 }
