f1d19d3b8cbdac9659d2983b36b7ec6b262e5ffe
luizlealdev <luizandreleal100@gmail.com>
2024-06-07 20:58:32 -0300

    feat: viewpager dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.picasso)
+    implementation(libs.androidx.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
