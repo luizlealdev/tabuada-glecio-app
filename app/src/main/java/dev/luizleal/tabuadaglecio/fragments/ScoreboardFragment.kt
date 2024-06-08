5640bfc9ccbeb63b7e50db12cb00c64f17a2e6f4
luizlealdev <luizandreleal100@gmail.com>
2024-06-07 21:00:59 -0300

    feat: LeaderboardActivity tab layout

---
+++ app/src/main/java/dev/luizleal/tabuadaglecio/fragments/ScoreboardFragment.kt
@@ -0,0 +1,99 @@
+package dev.luizleal.tabuadaglecio.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import dev.luizleal.tabuadaglecio.R
+import dev.luizleal.tabuadaglecio.adapter.LeaderboardUserAdapter
+import dev.luizleal.tabuadaglecio.databinding.FragmentScoreboardBinding
+import dev.luizleal.tabuadaglecio.model.LeaderboardUser
+
+class ScoreboardFragment : Fragment(R.layout.fragment_scoreboard) {
+    private var scoreboardBinding: FragmentScoreboardBinding? = null
+    private val binding get() = scoreboardBinding!!
+
+    private var firebaseDatabase: FirebaseDatabase? = null
+    private var databaseReference: DatabaseReference? = null
+    private var leaderboardUserList = mutableListOf<LeaderboardUser>()
+
+    private var adapter: LeaderboardUserAdapter? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        scoreboardBinding = FragmentScoreboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase?.getReference("leaderboard")
+
+        getData()
+
+        initRecyclerView()
+    }
+
+
+    private fun initRecyclerView() {
+        adapter = LeaderboardUserAdapter()
+        binding.apply {
+            recyclerLeaderboard.layoutManager = LinearLayoutManager(requireContext())
+            recyclerLeaderboard.adapter = adapter
+        }
+    }
+
+    private fun getData() {
+        databaseReference?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                leaderboardUserList.clear()
+
+                for (data in snapshot.children) {
+                    val id = data.key
+                    val username = data.child("username").value.toString()
+                    val userClass = data.child("userClass").value.toString()
+                    val avatarId = data.child("avatarId").value.toString()
+                    val score = data.child("score").value.toString()
+
+                    leaderboardUserList.add(
+                        LeaderboardUser(
+                            username = username,
+                            userClass = userClass,
+                            avatarId = avatarId,
+                            score = score.toInt()
+                        )
+                    )
+                }
+                Log.d("snapshot", leaderboardUserList.toString())
+
+                val sortedLeaderboardList: List<LeaderboardUser> = leaderboardUserList.sortedByDescending { it.score }
+                adapter?.setItems(sortedLeaderboardList.toMutableList())
+
+                binding.loadingSpinner.visibility = View.GONE
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Get Data Error", "onCanceled: ${error.toException()}")
+
+                Toast.makeText(
+                    requireContext(),
+                    "Ocorreu um erro, verifique sua conexão com a internet e tente novamente.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+    }
+}
--- app/src/main/java/dev/luizleal/tabuadaglecio/ui/LeaderboardActivity.kt
+++ app/src/main/java/dev/luizleal/tabuadaglecio/ui/LeaderboardActivity.kt
@@ -1,33 +1,15 @@
 package dev.luizleal.tabuadaglecio.ui
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import dev.luizleal.tabuadaglecio.R
-import dev.luizleal.tabuadaglecio.adapter.LeaderboardUserAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import dev.luizleal.tabuadaglecio.adapter.LeaderboardViewPagerAdapter
 import dev.luizleal.tabuadaglecio.databinding.ActivityLeaderboardBinding
-import dev.luizleal.tabuadaglecio.model.LeaderboardUser
 
 class LeaderboardActivity : AppCompatActivity() {
 
     private var leaderboardBinding: ActivityLeaderboardBinding? = null
     private val binding get() = leaderboardBinding!!
-
-    private var firebaseDatabase: FirebaseDatabase? = null
-    private var databaseReference: DatabaseReference? = null
-    private var leaderboardUserList = mutableListOf<LeaderboardUser>()
-
-    private var adapter: LeaderboardUserAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,68 +17,27 @@ class LeaderboardActivity : AppCompatActivity() {
         leaderboardBinding = ActivityLeaderboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-
-        firebaseDatabase = FirebaseDatabase.getInstance()
-        databaseReference = firebaseDatabase?.getReference("leaderboard")
-
-        getData()
-
-        initRecyclerView()
+        setupTabLayout()
     }
 
-
-    private fun initRecyclerView() {
-        adapter = LeaderboardUserAdapter()
-        binding.apply {
-            recyclerLeaderboard.layoutManager = LinearLayoutManager(applicationContext)
-            recyclerLeaderboard.adapter = adapter
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        leaderboardBinding = null
     }
 
-    private fun getData() {
-        databaseReference?.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                leaderboardUserList.clear()
+    private fun setupTabLayout() {
+        val tabLayout = binding.tabLeaderboardType
+        val viewPager = binding.viewpagerLeaderboard
 
-                for (data in snapshot.children) {
-                    val id = data.key
-                    val username = data.child("username").value.toString()
-                    val userClass = data.child("userClass").value.toString()
-                    val avatarId = data.child("avatarId").value.toString()
-                    val score = data.child("score").value.toString()
+        val viewPagerAdapter = LeaderboardViewPagerAdapter(this@LeaderboardActivity)
+        viewPager.adapter = viewPagerAdapter
 
-                    leaderboardUserList.add(
-                        LeaderboardUser(
-                            username = username,
-                            userClass = userClass,
-                            avatarId = avatarId,
-                            score = score.toInt()
-                        )
-                    )
-                }
-                Log.d("snapshot", leaderboardUserList.toString())
-
-                val sortedLeaderboardList: List<LeaderboardUser> = leaderboardUserList.sortedByDescending { it.score }
-                adapter?.setItems(sortedLeaderboardList.toMutableList())
-
-                binding.loadingSpinner.visibility = View.GONE
+        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+            tab.text = when(position) {
+                0 -> "Placar Normal"
+                1 -> "Placar Global"
+                else -> null
             }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("Get Data Error", "onCanceled: ${error.toException()}")
-
-                Toast.makeText(
-                    applicationContext,
-                    "Ocorreu um erro, verifique sua conexão com a internet e tente novamente.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        })
+        }.attach()
     }
 }
